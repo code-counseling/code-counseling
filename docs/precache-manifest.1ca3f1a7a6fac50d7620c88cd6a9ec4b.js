a4aa7016cb7ec4e8fbc3e4cb644c2de94410f2e8
Rahel Lüthy <rahel.luethy@gmail.com>
2019-01-19 15:03:15 +0100

    Deploy '18cc319'

---
+++ docs/precache-manifest.1ca3f1a7a6fac50d7620c88cd6a9ec4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c19d03d737ba91ffaf8bb77518478aa7",
+    "url": "/static/media/portrait.c19d03d7.jpg"
+  },
+  {
+    "revision": "6622248877b2b94ca9d47e73f5e141f1",
+    "url": "/static/media/logo.66222488.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db4007315e178a9ed7d8",
+    "url": "/static/js/main.db400731.chunk.js"
+  },
+  {
+    "revision": "3ac11f3dcae78d32bf8b",
+    "url": "/static/js/1.3ac11f3d.chunk.js"
+  },
+  {
+    "revision": "0856e3bccc8f326ae34e7722998a6321",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02a8a371ed324820ba2290936ecf6c1a.js"
+  "/precache-manifest.1ca3f1a7a6fac50d7620c88cd6a9ec4b.js"
 );
 
 workbox.clientsClaim();
