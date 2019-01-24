bf58fe9325b52c699bc6f0b23eb95bee62aff715
Rahel Lüthy <rahel.luethy@gmail.com>
2019-01-24 18:01:00 +0100

    Deploy fe7c0c9

---
+++ docs/precache-manifest.ce4672b7fd4e0ae770964e8a9edcfda9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84fffe5d4050231a14f3",
+    "url": "/static/js/main.84fffe5d.chunk.js"
+  },
+  {
+    "revision": "cae8f5d3b72ae3ccef39",
+    "url": "/static/js/1.cae8f5d3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6622248877b2b94ca9d47e73f5e141f1",
+    "url": "/static/media/logo.66222488.svg"
+  },
+  {
+    "revision": "c19d03d737ba91ffaf8bb77518478aa7",
+    "url": "/static/media/portrait.c19d03d7.jpg"
+  },
+  {
+    "revision": "a61320f18f4f83fd77d9e6fa006a116a",
+    "url": "/index.html"
+  }
+];
